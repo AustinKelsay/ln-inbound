a6e074ed5faf22a8f8c3e152d35c432a9768f8c9
Austin Kelsay <austinkelsay@yahoo.com>
2023-04-29 21:18:04 -0500

    Adding invoice QR, updating amount, normalizing some styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Connect from "@/components/Connect";
 import Amount from "@/components/Amount";
+import Invoice from "@/components/Invoice";
 
 export default function Home() {
+  const [invoicePolling, setInvoicePolling] = useState(false);
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-even p-24`}
+      className={`flex min-h-screen flex-col items-center justify-even p-12`}
     >
       <h1>Hello lninbound</h1>
       <Connect />
-      <Amount />
+      <Amount
+        invoicePolling={invoicePolling}
+        setInvoicePolling={setInvoicePolling}
+      />
+      {invoicePolling && <Invoice setInvoicePolling={setInvoicePolling} />}
     </main>
   );
 }
