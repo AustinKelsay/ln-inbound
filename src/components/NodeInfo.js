9b3723d8acf8421dc0c0cdde92f1a68fa38e8765
Austin Kelsay <austinkelsay@yahoo.com>
2023-04-30 12:10:54 -0500

    Added copy button

--- src/components/NodeInfo.js
+++ src/components/NodeInfo.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "@chakra-ui/react";
 
 const NodeInfo = () => {
   const [nodeInfo, setNodeInfo] = useState(null);
@@ -13,14 +14,31 @@ const NodeInfo = () => {
     fetchData();
   }, []);
 
+  const copyToClipboard = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert("Copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
   return (
     <div className="text-xl">
       {nodeInfo ? (
-        nodeInfo.map((info) => (
+        nodeInfo.map((info, index) => (
           <div className="w-2/4 m-auto" key={info}>
             <span className="font-bold">Node URI</span>
             <br />
             {info}
+            <Button
+              size="sm"
+              colorScheme="blue"
+              onClick={() => copyToClipboard(info)}
+              mt={1}
+            >
+              Copy
+            </Button>
           </div>
         ))
       ) : (
