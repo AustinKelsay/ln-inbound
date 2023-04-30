a0608880262e195e842e6a5f195b451640bc1bf0
Austin Kelsay <austinkelsay@yahoo.com>
2023-04-30 00:32:09 -0500

    Fixing duplicate import

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,17 @@ import Amount from "@/components/Amount";
 import Invoice from "@/components/Invoice";
 import Trail from "@/components/Trail";
 import PendingChannel from "@/components/PendingChannel";
+import NodeInfo from "@/components/NodeInfo";
 import { useSelector } from "react-redux";
-import Trail from "@/components/Trail"
-import NodeInfo from "@/components/NodeInfo"
 
 const mockNodeInfo = {
-    "identity_pubkey": "03298ea87f45bd5294f8052948564b68da378fa640267c7955128f35ee1ad6a064",
-    "alias": "alice",
-    "uris": [
-      "03298ea87f45bd5294f8052948564b68da378fa640267c7955128f35ee1ad6a064@172.20.0.4:9735"
-    ],  
-}
+  identity_pubkey:
+    "03298ea87f45bd5294f8052948564b68da378fa640267c7955128f35ee1ad6a064",
+  alias: "alice",
+  uris: [
+    "03298ea87f45bd5294f8052948564b68da378fa640267c7955128f35ee1ad6a064@172.20.0.4:9735",
+  ],
+};
 
 export default function Home() {
   const invoicePolling = useSelector((state) => state.polling);
@@ -32,16 +32,24 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-even p-12`}
     >
-      <div className={`w-full`} style={{ minHeight: 50, display: 'flex', justifyContent: 'center', marginBottom: '2rem' }}>
+      <div
+        className={`w-full`}
+        style={{
+          minHeight: 50,
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "2rem",
+        }}
+      >
         <Trail open={open}>
           <span className={`text-5xl font-bold`}>LN Inbound</span>
         </Trail>
-      </div> 
-      
+      </div>
+
       <NodeInfo {...mockNodeInfo} />
-      
+
       <Connect />
-      
+
       <Amount />
 
       {invoicePolling && <Invoice />}
