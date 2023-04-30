c85585c8271032bf5ec3e6afa6a61f60d1153c88
Austin Kelsay <austinkelsay@yahoo.com>
2023-04-29 20:40:45 -0500

    Basic connect component basic slider

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,12 +1,80 @@
-import React from "react";
-import { Button } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
-export default function Connect() {
+const Connect = () => {
+  const [nodePubkey, setNodePubkey] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Replace with your API endpoint
+      //   const response = await fetch("https://your-api-endpoint.com", {
+      //     method: "POST",
+      //     body: JSON.stringify({ nodePubkey }),
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //   });
+
+      const response = {
+        ok: false,
+      };
+
+      if (response.ok) {
+        setMessage("Connection successful!");
+        setMessageType("success");
+      } else {
+        setMessage("Connection failed");
+        setMessageType("error");
+      }
+
+      setTimeout(() => {
+        setMessage("");
+        setMessageType("");
+      }, 6000);
+    } catch (error) {
+      setMessage("An error occurred. Please try again.");
+      setMessageType("error");
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-      <Button colorScheme="blue">Connect</Button>
+    <main className={`flex flex-col items-center justify-start p-24`}>
+      <form onSubmit={handleSubmit}>
+        <VStack spacing={4}>
+          <FormControl id="node-pubkey">
+            <FormLabel>Node Pubkey</FormLabel>
+            <Input
+              type="text"
+              value={nodePubkey}
+              onChange={(e) => setNodePubkey(e.target.value)}
+            />
+          </FormControl>
+          <Button type="submit" colorScheme="blue">
+            Connect
+          </Button>
+          {message && (
+            <Text
+              fontWeight="bold"
+              color={messageType === "success" ? "green.500" : "red.500"}
+            >
+              {message}
+            </Text>
+          )}
+        </VStack>
+      </form>
     </main>
   );
-}
+};
+
+export default Connect;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
-import React from "react"
+import React from "react";
+import Connect from "@/components/Connect";
+import Amount from "@/components/Amount";
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+      className={`flex min-h-screen flex-col items-center justify-even p-24`}
     >
       <h1>Hello lninbound</h1>
+      <Connect />
+      <Amount />
     </main>
-  )
+  );
 }
