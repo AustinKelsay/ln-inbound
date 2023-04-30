8412b206b12a8775190534ebf0a3a8d00951f7c1
Austin Kelsay <austinkelsay@yahoo.com>
2023-04-29 20:40:45 -0500

    Basic connect component basic slider

---
+++ src/components/Amount.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderMark,
+} from "@chakra-ui/react";
+
+const Amount = () => {
+  const [sliderValue, setSliderValue] = useState(50);
+
+  const labelStyles = {
+    mt: "2",
+    ml: "-2.5",
+    fontSize: "sm",
+  };
+
+  return (
+    <Box w={"100%"}>
+      <Slider aria-label="slider-ex-6" onChange={(val) => setSliderValue(val)}>
+        <SliderMark value={25} {...labelStyles}>
+          25%
+        </SliderMark>
+        <SliderMark value={50} {...labelStyles}>
+          50%
+        </SliderMark>
+        <SliderMark value={75} {...labelStyles}>
+          75%
+        </SliderMark>
+        <SliderMark
+          value={sliderValue}
+          textAlign="center"
+          bg="blue.500"
+          color="white"
+          mt="-10"
+          ml="-5"
+          w="12"
+        >
+          {sliderValue}%
+        </SliderMark>
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+    </Box>
+  );
+};
+
+export default Amount;
