15e470bc6f1343168164084fdf806af6e0589fd3
Austin Kelsay <austinkelsay@yahoo.com>
2023-04-29 20:14:07 -0500

    Setting up chakra

---
+++ src/components/Connect.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+export default function Connect() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+    >
+      <Button colorScheme="blue">Connect</Button>
+    </main>
+  );
+}
